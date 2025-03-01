f510be27e24824006413ce5dfef4e1b3d3af1727
Lucas <lucasackerberg@github.com>
2025-03-01 22:03:10 +0100

    init

---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { Head, Link } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="Welcome">
+        <link rel="preconnect" href="https://rsms.me/" />
+        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    </Head>
+    <div class="flex min-h-screen flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] dark:bg-[#0a0a0a] lg:justify-center lg:p-8">
+        <video
+            src="/videos/backgroundvideo.mp4"
+            autoplay
+            muted
+            loop
+            class="blur-xs absolute inset-0 z-0 h-full w-full object-cover opacity-35"
+        ></video>
+        <div class="blur-xs flex h-full w-full bg-transparent"></div>
+        <header class="not-has-[nav]:hidden z-10 mb-6 w-full max-w-[335px] text-sm lg:max-w-4xl">
+            <nav class="flex items-center justify-end gap-4">
+                <Link
+                    v-if="$page.props.auth.user"
+                    :href="route('dashboard')"
+                    class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                >
+                    Dashboard
+                </Link>
+                <template v-else>
+                    <Link
+                        :href="route('login')"
+                        class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
+                    >
+                        Log in
+                    </Link>
+                    <Link
+                        :href="route('register')"
+                        class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
+                    >
+                        Register
+                    </Link>
+                </template>
+            </nav>
+        </header>
+        <div class="duration-750 starting:opacity-0 z-10 flex w-full items-start justify-center opacity-100 transition-opacity lg:grow">
+            <main class="mt-32 flex w-full max-w-[335px] flex-col flex-wrap rounded-lg lg:max-w-4xl lg:flex-row">
+                <h2 class="text-5xl font-bold text-[#1b1b18] dark:text-[#EDEDEC]">P 0 K E R H A N D S</h2>
+                <p class="mt-4 text-lg text-[#1b1b18] dark:text-[#EDEDEC]">
+                    A simple poker hand evaluator that can evaluate a poker hand amongst others and determine its rank and percentage to win.
+                </p>
+                <p class="mt-4 text-lg text-[#1b1b18] dark:text-[#EDEDEC]">
+                    You also have the possibility to save your hands with an account if you wish!
+                </p>
+            </main>
+            <main>
+                <!-- animate two poker cards coming in from the right -->
+            </main>
+        </div>
+        <div class="h-14.5 hidden lg:block"></div>
+    </div>
+</template>
